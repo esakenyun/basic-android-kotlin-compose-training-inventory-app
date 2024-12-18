5406300217bd95ffaabaf12dabf7d151ae677439
esakenyun <rfahlevih@gmail.com>
2024-12-18 19:52:02 +0700

    Update Lanjutan Modul 10

--- app/src/main/java/com/example/inventory/ui/AppViewModelProvider.kt
+++ app/src/main/java/com/example/inventory/ui/AppViewModelProvider.kt
@@ -36,7 +36,9 @@ object AppViewModelProvider {
         // Initializer for ItemEditViewModel
         initializer {
             ItemEditViewModel(
-                this.createSavedStateHandle()
+                this.createSavedStateHandle(),
+                inventoryApplication().container.itemsRepository
+
             )
         }
         // Initializer for ItemEntryViewModel
@@ -47,13 +49,14 @@ object AppViewModelProvider {
         // Initializer for ItemDetailsViewModel
         initializer {
             ItemDetailsViewModel(
-                this.createSavedStateHandle()
+                this.createSavedStateHandle(),
+                inventoryApplication().container.itemsRepository
             )
         }
 
         // Initializer for HomeViewModel
         initializer {
-            HomeViewModel()
+            HomeViewModel(inventoryApplication().container.itemsRepository)
         }
     }
 }
